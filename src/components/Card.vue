a078c678287b65a2285861953a19a37b3df54396
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-03-03 17:04:32 +0100

    FEAT store added

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,8 @@
 <template>
-    <li>
+    <li class="card-box">
         <img :src="card.card_images[0].image_url" alt="">
         <p>{{card.name}}</p>
+        <span>{{ card.archetype }}</span>
     </li>
 </template>
 
@@ -17,4 +18,26 @@ export default{
 </script>
 
 <style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+.card-box{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+    background-color: $bg-color-primary;
+
+    p{
+        font-size: 1.2rem;
+        text-align: center;
+        color: white;
+    }
+
+    span{
+        margin-top: auto;
+        font-weight: 600;
+        margin-bottom: 10px;
+    }
+    
+}
+
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <ul class="card-list">
                 
-                <Card v-for="card in cards" :key="card.id" :card="card" />
+                <Card v-for="card in store.cards" :key="card.id" :card="card" />
                 
             </ul>
         </div>
@@ -11,8 +11,12 @@
 </template>
 
 <script>
+// Axios serve per le api
 import axios from 'axios';
+// Card è un componente
 import Card from './Card.vue';
+// store è dove si trovano le info date dalle api
+import store from '../store';
 
 export default{
     components:{
@@ -20,7 +24,8 @@ export default{
     },
     data(){
         return{
-            cards: [],
+            // cards: [],
+            store,
         }
     },
     methods:{
@@ -32,7 +37,8 @@ export default{
                 // console.log(res.data);
                 // console.log(res.data.data);
 
-                this.cards = res.data.data;
+                // this.cards = res.data.data;
+                this.store.cards = res.data.data;
 
             })
         }
@@ -49,6 +55,7 @@ export default{
 
 .main{
     padding: 40px;
+    background-image: url('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/084d243d-e248-4e6c-8877-21895886b8bb/d7xwheu-a54f38fb-ac20-48d5-a7c8-893ccd06701b.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzA4NGQyNDNkLWUyNDgtNGU2Yy04ODc3LTIxODk1ODg2YjhiYlwvZDd4d2hldS1hNTRmMzhmYi1hYzIwLTQ4ZDUtYTdjOC04OTNjY2QwNjcwMWIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.5lDicK3YzccLUhMNmIbm52s0J54ukQozFesqL3X2ojE');
 }
 
 .card-list{
